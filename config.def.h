4affddbb4b575f4558a09cbf28d2cebcea2d1418
zrzf <79478101+zrzf@users.noreply.github.com>
2021-03-25 22:10:40 -0300

    Update config.def.h

--- config.def.h
+++ config.def.h
@@ -6,10 +6,10 @@ static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=14:style=bold" };
+static const char *fonts[]          = { "Hack:size=14" };
 //static const char *fonts[]          = { "fontawesome-regular:size=16:style=bold" };
 //static const char dmenufont[]       = "fontawesome-regular:size=16:style=bold";
-static const char dmenufont[]       = "monospace:size=14:style=bold";
+static const char dmenufont[]       = "Hack:size=14";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
