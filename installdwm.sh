1a6f027d86a4db1e02518e2d7319cc59ea36a86b
jvmf1 <79478101+jvmf1@users.noreply.github.com>
2021-11-28 01:48:02 -0300

    Add files via upload

---
+++ installdwm.sh
@@ -0,0 +1,31 @@
+git clone https://github.com/jvmf1/dwm
+cd dwm
+sudo make install
+cd ..
+
+git clone https://git.suckless.org/dmenu
+cd dmenu
+sudo make install
+cd ..
+
+git clone https://github.com/jvmf1/dwmstatus
+cd dwmstatus
+sudo make install
+cd ..
+
+pacman -S stalonetray nm-applet terminus-font xfce4-terminal firefox
+
+cat > ~/.xinitrc << EOF
+(stalonetray) &
+(nm-applet) &
+dwmstatus &
+xset r rate 250 40 &
+setxkbmap br &
+exec dwm
+EOF
+
+cat > ~/.config/gtk-3.0/settings.ini << EOF
+[Settings]
+gtk-theme-name = Adwaita
+gtk-application-prefer-dark-theme = true
+EOF
